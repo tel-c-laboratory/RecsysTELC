262ead77f1e05a37371fc4e19683b8112a1318eb
Fakhri Fauzan <fazan697@gmail.com>
2019-01-27 19:47:35 +0700

    add symlink

---
+++ symlink.php
@@ -0,0 +1,8 @@
+<?php
+  $linkName = '/home/u319770187/public_html/recsys/storage';
+  $target = '/home/u319770187/public_html/recruitment/storage/app/public';
+
+  symlink($target, $linkName);
+
+  // ln -s /home/u319770187/public_html/recruitment/storage/app/public /home/u319770187/public_html/recsys/storage
+?>
